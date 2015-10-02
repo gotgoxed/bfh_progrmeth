c245744640fa3615baf1c34b31507492cb5f05ea
bananatreedad <jnt.meier@gmail.com>
2015-10-02 08:20:31 +0200

    add resourse files

--- 02Uebung/src/main/java/ch/bnntd/bfh/pgrm/filesexc/PersonDataProcessor.java
+++ 02Uebung/src/main/java/ch/bnntd/bfh/pgrm/filesexc/PersonDataProcessor.java
@@ -9,6 +9,12 @@
 package ch.bnntd.bfh.pgrm.filesexc;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author jnt
@@ -16,30 +22,86 @@ import java.io.File;
  */
 public class PersonDataProcessor {
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(PersonDataProcessor.class);
+
 	/**
 	 * @param inputFile
-	 * - name of the file which should be read. Must always be defined 
-	 * and can't be an empty string ("") or null.
+	 *            - name of the file which should be read. Must always be
+	 *            defined and can't be an empty string ("") or null.
 	 * 
 	 * @param outputFileName
-	 * - name of the file to write the correct lines to. 
-	 * If the name is defined as an empty string (""), no file is created.
+	 *            - name of the file to write the correct lines to. If the name
+	 *            is defined as an empty string (""), no file is created.
 	 * 
 	 * @param outputErrorFileName
-	 * - name of the file to write the wrong lines to. 
-	 * If the name is defined as an empty string (""), no file is created.
+	 *            - name of the file to write the wrong lines to. If the name is
+	 *            defined as an empty string (""), no file is created.
 	 * 
 	 * @param logFileName
-	 * - the name of the file to which the log should be written to. 
-	 * If the name is defined as an empty string (""), no file is created.
+	 *            - the name of the file to which the log should be written to.
+	 *            If the name is defined as an empty string (""), no file is
+	 *            created.
 	 * 
-	 * Die Methode gibt true zurück, wenn alle Zeilen der Input-Daten das 
-	 * geforderte Format erfüllen.
+	 *            Die Methode gibt true zurück, wenn alle Zeilen der
+	 *            Input-Daten das geforderte Format erfüllen.
 	 * 
 	 */
 	public static boolean dataAnalyzer(File inputFile, File outputFile,
 			File outputErrorFile, File logFile) {
-		
+
+		Scanner inputFileScanner = null;
+		PrintWriter outputFileWriter = null;
+		PrintWriter outputErrorFileWriter = null;
+		PrintWriter logFileWriter = null;
+
+		try {
+
+			inputFileScanner = new Scanner(inputFile);
+			outputFileWriter = new PrintWriter(outputFile);
+			outputErrorFileWriter = new PrintWriter(outputErrorFile);
+			logFileWriter = new PrintWriter(logFile);
+
+			if (inputFileScanner.hasNext(".")) {
+				while (inputFileScanner.hasNextLine()) {
+					String line = inputFileScanner.nextLine();
+					if (checkLine(line)) {
+						outputFileWriter.append(line);
+					} else {
+						outputErrorFileWriter.append(line);
+					}
+				}
+
+			} else
+				throw new EmptyFileException();
+
+		} catch (FileNotFoundException e) {
+			logger.error(e.getMessage());
+			return false;
+		}
+		finally {
+			if(inputFileScanner != null) inputFileScanner.close();
+			if(inputFileScanner != null) outputFileWriter.close();
+			if(inputFileScanner != null) outputErrorFileWriter.close();
+			if(inputFileScanner != null) logFileWriter.close();
+		}
+
 		return false;
+	}
+
+	private static boolean checkLine(String line) {
+
+		return false;
+	}
+
+	/**
+	 * Thrown to indicate that a method tried to work with a File which is
+	 * empty.
+	 * 
+	 * @author jnt
+	 *
+	 */
+	public static class EmptyFileException extends IllegalArgumentException {
+
 	}
 }
--- 02Uebung/src/test/java/ch/bnntd/bfh/pgrm/filesexc/PersonDataProcessorTest.java
+++ 02Uebung/src/test/java/ch/bnntd/bfh/pgrm/filesexc/PersonDataProcessorTest.java
@@ -2,7 +2,13 @@ package ch.bnntd.bfh.pgrm.filesexc;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Scanner;
 
 import org.junit.Test;
 
@@ -17,8 +23,36 @@ public class PersonDataProcessorTest {
 
 		boolean testFile = PersonDataProcessor.dataAnalyzer(inputFile,
 				outputFile, outputErrorFile, logFile);
-		
+
 		assertFalse(testFile);
 	}
 
+	@Test
+	public void testResourcePath() {
+
+		System.out.println(this.getClass().getResource("."));
+
+		Scanner scanner;
+		try {
+			File newFile = new File(PersonDataProcessor.class.getResource(
+					"DataCSVerrors2.txt").toURI());
+
+			System.out.println(newFile.toString());
+
+			scanner = new Scanner(newFile, "UTF-8");
+
+			while (scanner.hasNextLine()) {
+				System.out.println("test");
+				System.out.println(scanner.nextLine());
+			}
+
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.getMessage();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
