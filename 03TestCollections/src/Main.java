564236cf570d52ccf8e34931e0805a19877f0b93
bananatreedad <jnt.meier@gmail.com>
2015-10-08 14:35:14 +0200

    add TestCollection

---
+++ 03TestCollections/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> intList = new ArrayList<Integer>();
+		intList.add(2);
+		//int --> Integer, automated by Java
+	}
+
+}
