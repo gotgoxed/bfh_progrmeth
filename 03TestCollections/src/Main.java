4106067988d9342a7fdb21702f23deba9d584fd5
bananatreedad <jnt.meier@gmail.com>
2015-10-08 14:39:12 +0200

    add .set and .get, as add at index tests

--- 03TestCollections/src/Main.java
+++ 03TestCollections/src/Main.java
@@ -1,13 +1,22 @@
 import java.util.ArrayList;
 
-
 public class Main {
 
 	public static void main(String[] args) {
 
 		ArrayList<Integer> intList = new ArrayList<Integer>();
 		intList.add(2);
-		//int --> Integer, automated by Java
-	}
+		// int --> Integer, automated by Java
 
+		intList.add(3);
+		intList.add(4);
+		intList.add(5);
+
+		System.out.println(intList);
+		intList.set(1, 8);
+		System.out.println(intList);
+		intList.add(1, 10);
+		System.out.println(intList);
+
+	}
 }
